85e908eeb505c4c88800d439f0a9b215de90285a
hamidreza <dolati.hamidreza78@gmail.com>
2025-07-15 22:58:42 +0330

    simple command line commmits ping for multiple websites

--- ping.py
+++ ping.py
@@ -1,7 +1,13 @@
+import argparse
 import asyncio
 import httpx
-import sys
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=" Ping one or multiple URL(s) and get the average response time for each website")
+    parser.add_argument("url", help="URL(s) to ping; seperate multiple URLs with a comma")
+    
+    return parser.parse_args()
 
 async def get_ping(client: httpx.AsyncClient, url:str):
     print(f"[*] Pinging {url}...")
@@ -10,16 +16,16 @@ async def get_ping(client: httpx.AsyncClient, url:str):
 
     return response.url, response.elapsed
 
-
 async def main():
-    url = "https://google.com"
+    args = parse_args()
     async with httpx.AsyncClient() as client:
         async with asyncio.TaskGroup() as tg:
-            tasks = [tg.create_task(get_ping(client, url)) for _ in range(3)]
+            tasks = {url:[tg.create_task(get_ping(client, url)) for _ in range(3)] for url in args.url.split(",")}
         for task in tasks:
             url, elapsed = task.result()
             print(url, ":", elapsed)
 
+#todo : get muliple urls from command line arguments
 
 if __name__ == "__main__":
     asyncio.run(main())
